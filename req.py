3ee143312b89235181c53d6bc172ce2460dcc865
Ragnoji <58487417+Ragnoji@users.noreply.github.com>
2022-04-20 18:13:33 +0300

    Initial project release

---
+++ req.py
@@ -0,0 +1,77 @@
+import requests
+from re import search
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import vlc
+import pickle
+
+
+
+def check_case_update(page=1):
+    response = requests.get(
+        f'https://blog.counter-strike.net/index.php/category/updates/page/{page}/',
+    )
+    text = response.text
+    recent_post = search(r'the [A-Z].+Case', text)
+    box_name = recent_post[0].replace('#038;', '')[4:] if recent_post else False
+    return box_name
+
+def loop_alarm():
+    p = vlc.MediaPlayer("alarm.mp3")
+    p.play()
+    while True:
+        sleep(1)
+        if not p.is_playing():
+            p = vlc.MediaPlayer("alarm.mp3")
+            p.play()
+
+
+if __name__ == '__main__':
+    options = webdriver.ChromeOptions()
+
+    binary_yandex_driver_file = 'yandexdriver.exe'
+
+    driver = webdriver.Chrome(binary_yandex_driver_file, options=options)
+
+    req_time = 15
+    print('Checking for case update...')
+    new_box_name = check_case_update(2)
+    while not new_box_name:
+        sleep(req_time)
+        print('Checking for case update...')
+        new_box_name = check_case_update()
+
+    url = f'https://steamcommunity.com/market/listings/730/{new_box_name}'
+    console_command = f'Market_ShowBuyOrderPopup(730, "{new_box_name}", "{new_box_name}")'
+    print(f'NEW CASE RELEASE\nCommand for fast buy:\n{console_command}')
+    driver.get(url)
+
+    for cookie in pickle.load(open('steam_cookies', 'rb')):
+        driver.add_cookie(cookie)
+
+    while True:
+        driver.refresh()
+        sleep(1)
+
+        driver.execute_script(f'Market_ShowBuyOrderPopup(730, "{new_box_name}", "{new_box_name}")')
+        price = driver.find_element_by_xpath('//*[@id="market_buy_commodity_input_price"]')
+        price.send_keys(Keys.BACKSPACE*50, '1')
+        sleep(0.1)
+        quantity = driver.find_element_by_xpath('//*[@id="market_buy_commodity_input_quantity"]')
+        quantity.send_keys(Keys.BACKSPACE*50,'2')
+        sleep(0.1)
+        accept = driver.find_element_by_xpath('//*[@id="market_buyorder_dialog_accept_ssa"]')
+        accept.click()
+        sleep(0.1)
+        place = driver.find_element_by_xpath('//*[@id="market_buyorder_dialog_purchase"]')
+        place.click()
+        sleep(15)
+
+        is_error = driver.find_element_by_id('market_buyorder_dialog_error_text').text
+        print(is_error)
+        if not is_error or is_error == 'You already have an active buy order for this item. You will need to either cancel that order, or wait for it to be fulfilled before you can place a new order.':
+            break
+
+    loop_alarm()
+
---
+++ steamauth.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from steam_data import steam_login, steam_password
+import time
+import pickle
+
+options = webdriver.ChromeOptions()
+
+binary_yandex_driver_file = 'yandexdriver.exe'
+
+driver = webdriver.Chrome(binary_yandex_driver_file, options=options)
+
+try:
+    url = 'https://steamcommunity.com/login/home/?goto=market'
+    driver.get(url)
+
+    login_input = driver.find_element_by_id("input_username")
+    login_input.clear()
+    login_input.send_keys(steam_login)
+    time.sleep(1)
+
+    password_input = driver.find_element_by_id("input_password")
+    password_input.clear()
+    password_input.send_keys(steam_password)
+    time.sleep(1)
+
+    login_button = driver.find_element_by_xpath('//*[@id="login_btn_signin"]/button').click()
+    time.sleep(3)
+
+    sda_input = driver.find_element_by_xpath('//*[@id="twofactorcode_entry"]')
+    sda_input.send_keys(input('Введите SteamGuard:\n'))
+    sda_button = driver.find_element_by_xpath('//*[@id="login_twofactorauth_buttonset_entercode"]/div[1]').click()
+    time.sleep(5)
+
+    pickle.dump(driver.get_cookies(), open('steam_cookies', 'wb'))
+
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
