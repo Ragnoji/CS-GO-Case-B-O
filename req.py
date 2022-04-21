62b7b813934d433e028c5ba39c7b2f3b944d92ff
Ragnoji <58487417+Ragnoji@users.noreply.github.com>
2022-04-21 19:15:37 +0300

    Nothing

--- req.py
+++ req.py
@@ -7,15 +7,20 @@ import vlc
 import pickle
 
 
-
 def check_case_update(page=1):
     response = requests.get(
         f'https://blog.counter-strike.net/index.php/category/updates/page/{page}/',
     )
     text = response.text
-    recent_post = search(r'the [A-Z].+Case', text)
-    box_name = recent_post[0].replace('#038;', '')[4:] if recent_post else False
+    recent_post = search(r'the .*[A-Z].+Case', text)
+    box_name = False
+    if recent_post:
+        box_name = recent_post[0].replace('#038;', '')[4:]
+        box_name = box_name[:box_name.find('Case') + 4]
+        box_name = ' '.join(filter(lambda w: w[0].isupper() or w[0].isdigit() or w[0] == '&', box_name.split()))
+
     return box_name
+
 
 def loop_alarm():
     p = vlc.MediaPlayer("alarm.mp3")
@@ -34,9 +39,9 @@ if __name__ == '__main__':
 
     driver = webdriver.Chrome(binary_yandex_driver_file, options=options)
 
-    req_time = 15
+    req_time = 10
     print('Checking for case update...')
-    new_box_name = check_case_update(2)
+    new_box_name = check_case_update()
     while not new_box_name:
         sleep(req_time)
         print('Checking for case update...')
@@ -46,7 +51,6 @@ if __name__ == '__main__':
     console_command = f'Market_ShowBuyOrderPopup(730, "{new_box_name}", "{new_box_name}")'
     print(f'NEW CASE RELEASE\nCommand for fast buy:\n{console_command}')
     driver.get(url)
-
     for cookie in pickle.load(open('steam_cookies', 'rb')):
         driver.add_cookie(cookie)
 
@@ -56,10 +60,14 @@ if __name__ == '__main__':
 
         driver.execute_script(f'Market_ShowBuyOrderPopup(730, "{new_box_name}", "{new_box_name}")')
         price = driver.find_element_by_xpath('//*[@id="market_buy_commodity_input_price"]')
-        price.send_keys(Keys.BACKSPACE*50, '1')
+        balance_element = driver.find_element_by_xpath('//*[@id="market_buyorder_dialog_walletbalance_amount"]').text
+        balance = int(balance_element.split('руб')[0].split(',')[0])
+        cost = 120  # рублей
+        quant = balance // cost
+        price.send_keys(Keys.BACKSPACE*50, f'{cost}')
         sleep(0.1)
         quantity = driver.find_element_by_xpath('//*[@id="market_buy_commodity_input_quantity"]')
-        quantity.send_keys(Keys.BACKSPACE*50,'2')
+        quantity.send_keys(Keys.BACKSPACE*50, f'{quant}')
         sleep(0.1)
         accept = driver.find_element_by_xpath('//*[@id="market_buyorder_dialog_accept_ssa"]')
         accept.click()
