2cd56a7d48fa89230fc8d4e2414eb339d82bd860
Ragnoji <58487417+Ragnoji@users.noreply.github.com>
2022-04-22 15:37:33 +0300

    Made some important fixes

--- async_shit.py
+++ async_shit.py
@@ -17,20 +17,20 @@ def main():
 
     list_of_items = open('target_items.txt', 'r')
     list_of_items = [tuple(item_line.strip().split()) for item_line in list_of_items.readlines()]
+    print(*list_of_items)
     driver.get(url)
     for cookie in pickle.load(open('steam_cookies', 'rb')):
         driver.add_cookie(cookie)
     driver.refresh()
 
     index = 0
+    accept_terms_flag = True
     while list_of_items:
         item = list_of_items[index]
         name = item[0]
         cost = item[1]
         quant = item[2]
         console_command = f'Market_ShowBuyOrderPopup({GAME_INDEX}, "{name}", "{name}")'
-        driver.refresh()
-        sleep(1)
 
         driver.execute_script(console_command)
         price = driver.find_element_by_xpath('//*[@id="market_buy_commodity_input_price"]')
@@ -39,26 +39,30 @@ def main():
         quantity = driver.find_element_by_xpath('//*[@id="market_buy_commodity_input_quantity"]')
         quantity.send_keys(Keys.BACKSPACE * 50, f'{quant}')
         sleep(0.1)
-        accept = driver.find_element_by_xpath('//*[@id="market_buyorder_dialog_accept_ssa"]')
-        accept.click()
-        sleep(0.1)
+        if accept_terms_flag:
+            accept = driver.find_element_by_xpath('//*[@id="market_buyorder_dialog_accept_ssa"]')
+            accept.click()
+            sleep(0.1)
+            accept_terms_flag = False
         place = driver.find_element_by_xpath('//*[@id="market_buyorder_dialog_purchase"]')
         place.click()
         sleep(2)
         is_error = driver.find_element_by_id('market_buyorder_dialog_error_text').text
-        if not is_error or is_error == 'Sorry! We had trouble hearing back from the Steam servers about your order. Double check whether or not your order has actually been created or filled. If not, then please try again later.':
+        if not is_error or is_error == 'Sorry! We had trouble hearing back from the Steam servers about your order. Double check whether or not your order has actually been created or filled. If not, then please try again later.' or is_error == 'You cannot buy any items until your previous action completes.':
             index += 1
             if index == len(list_of_items):
                 index = 0
+            if is_error == 'You cannot buy any items until your previous action completes.':
+                driver.refresh()
             continue
         sleep(5)
 
         is_error = driver.find_element_by_id('market_buyorder_dialog_error_text').text
         print(is_error)
         if not is_error or is_error == 'You already have an active buy order for this item. You will need to either cancel that order, or wait for it to be fulfilled before you can place a new order.':
-            driver.execute_script('window.close();')
             del list_of_items[index]
             index -= 1
+            driver.refresh()
         index += 1
         if index == len(list_of_items):
             index = 0
--- req.py
+++ req.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import vlc
 import pickle
+from datetime import datetime
 
 
 def check_case_update(page=1):
@@ -40,15 +41,17 @@ if __name__ == '__main__':
 
     driver = webdriver.Chrome(binary_yandex_driver_file, options=options)
 
-    url = f'https://steamcommunity.com/market'
+    url = f'https://steamcommunity.com/market/listings/730/Dreams%20%26%20Nightmares%20Case'
     driver.get(url)
     for cookie in pickle.load(open('steam_cookies', 'rb')):
         driver.add_cookie(cookie)
     driver.refresh()
 
-    req_time = 10
+    # while datetime.now().time().hour != 0:
+    #    sleep(60)
+    req_time = 2
     print('Checking for case update...')
-    new_box_name = check_case_update()
+    new_box_name = 'check'  # check_case_update()
     while not new_box_name:
         sleep(req_time)
         print('Checking for case update...')
@@ -57,33 +60,38 @@ if __name__ == '__main__':
     url = f'https://steamcommunity.com/market/listings/730/{new_box_name}'
     console_command = f'Market_ShowBuyOrderPopup(730, "{new_box_name}", "{new_box_name}")'
     print(f'NEW CASE RELEASE\nCommand for fast buy:\n{console_command}')
-    driver.get(url)
-    driver.refresh()
+    # driver.get(url)
+    # driver.refresh()
+    accept_terms_flag = True
     while True:
         driver.execute_script(f'Market_ShowBuyOrderPopup(730, "{new_box_name}", "{new_box_name}")')
         price = driver.find_element_by_xpath('//*[@id="market_buy_commodity_input_price"]')
         balance_element = driver.find_element_by_xpath('//*[@id="market_buyorder_dialog_walletbalance_amount"]').text
         balance = int(balance_element.split('руб')[0].split(',')[0])
-        cost = 120  # рублей
+        cost = 130  # рублей
         quant = balance // cost
         price.send_keys(Keys.BACKSPACE*50, f'{cost}')
         sleep(0.1)
         quantity = driver.find_element_by_xpath('//*[@id="market_buy_commodity_input_quantity"]')
         quantity.send_keys(Keys.BACKSPACE*50, f'{quant}')
         sleep(0.1)
-        accept = driver.find_element_by_xpath('//*[@id="market_buyorder_dialog_accept_ssa"]')
-        accept.click()
-        sleep(0.1)
+
+        if accept_terms_flag:
+            accept = driver.find_element_by_xpath('//*[@id="market_buyorder_dialog_accept_ssa"]')
+            accept.click()
+            sleep(0.1)
+            accept_terms_flag = False
+
         place = driver.find_element_by_xpath('//*[@id="market_buyorder_dialog_purchase"]')
         place.click()
-        sleep(15)
-
+        sleep(5)
         is_error = driver.find_element_by_id('market_buyorder_dialog_error_text').text
-        print(is_error)
+        if not is_error or is_error == 'Sorry! We had trouble hearing back from the Steam servers about your order. Double check whether or not your order has actually been created or filled. If not, then please try again later.' or is_error == 'You cannot buy any items until your previous action completes.':
+            continue
+
+        sleep(10)
         if not is_error or is_error == 'You already have an active buy order for this item. You will need to either cancel that order, or wait for it to be fulfilled before you can place a new order.':
             break
-
-        driver.refresh()
 
     loop_alarm()
 
